49b1dd2dce545602363962144cd1b4ddfb731719
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2023-04-04 15:36:42 -0400

    Decision Tree Classifier on IRIS and MNIST
    - Ran and evaluated decision tree classifeir on IRIS and MNIST datasets

---
+++ IRIS_Tree.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    X,y = load_iris(return_X_y=True)
+
+    dtc = tree.DecisionTreeClassifier(criterion='entropy')
+
+    dtc.fit(X,y)
+
+    #  Evaluation
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.2, random_state=0)
+
+    y_pred = dtc.predict(X_test)
+
+    print(classification_report(y_test, y_pred))
+
+    print("Confusion matrix:\n", confusion_matrix(y_test, y_pred))
